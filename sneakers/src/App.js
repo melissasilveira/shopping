9af0bbaf57d4ff8d34da66f6c6c749802f501471
melissasilveira <melissaasilveira@gmail.com>
2022-05-31 17:46:23 -0300

    projeto iniciado

---
+++ sneakers/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <React.Fragment>
+      <div>
+        <h1>JAXONS</h1>
+      </div>
+      <div>
+        <h5>30% Discount on all products till 31st of december</h5>
+      </div>
+      <div>
+        <h3>Search results</h3>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ sneakers/src/sneakers.js
@@ -0,0 +1,30 @@
+export const sneakers = [
+  {
+    brand: 'Jordan',
+    description: 'Air Jordan 4 Retro Premium',
+    price: '$ 756',
+    image: '/sneakers/01.png',
+    icon: 'Fav Icon Selected.png',
+  },
+  {
+    brand: 'Jordan',
+    description: 'Air Jordan 4 Retro Fire Red',
+    price: '$ 657',
+    image: '/sneakers/02.png',
+    icon: 'Fav Icon.png',
+  },
+  {
+    brand: 'Jordan',
+    description: 'Air Jordan 4 Retro Raptor',
+    price: '$ 889',
+    image: '/sneakers/03.png',
+    icon: 'Fav Icon Selected.png',
+  },
+  {
+    brand: 'Jordan',
+    description: 'Air Jordan 4 Retro Raptor',
+    price: '$ 456',
+    image: '/sneakers/01.png',
+    icon: 'Fav Icon.png',
+  },
+]
